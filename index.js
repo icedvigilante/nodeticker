83843b63b5a0abbcf5280ba7b993fc572bbcdcc4
icedvigilante <wm.ryan.mooney@gmail.com>
2020-11-12 18:29:16 -0900

    Stock Searches now Working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app  = express();
 const request = require('request');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000;
 
 //API KEY: pk_d1bc1412c1f44ab8acd06840ad3f0e85
@@ -10,9 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+// use Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false}))
+
 // Configure API
-function call_api(finishedAPI) {
-    request('https://cloud.iexapis.com/stable/stock/bsrc/quote?token=pk_d1bc1412c1f44ab8acd06840ad3f0e85', { json: true}, (err, res, body) => {
+function call_api(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_d1bc1412c1f44ab8acd06840ad3f0e85', { json: true}, (err, res, body) => {
         if (err) { return console.log(err);}
         if(res.statusCode === 200){
             // console.log(body);
@@ -21,14 +25,24 @@ function call_api(finishedAPI) {
     });
 }
 
-
+// Set Handlebar Get Index Route
 app.get('/', function (req,res) {
     call_api(function(doneAPI){
         res.render('home', {
             stock: doneAPI
         });
-    });
+    }, 'fb');
+});
 
+// Set Handlebar Post Index Route
+app.post('/', function (req,res) {
+    call_api(function(doneAPI){
+        // posted_stuff = req.body.stock_ticker;
+        res.render('home', {
+            stock: doneAPI,
+            // posted_stuff: posted_stuff
+        });
+    },req.body.stock_ticker);
 });
 
 app.get('/about', (req, res) => {
